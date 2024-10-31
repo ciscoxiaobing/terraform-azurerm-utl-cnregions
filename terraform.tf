887a51313acbaf8ddb62cde8509e32d1e3ce393a
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-10-31 11:36:10 +0000

    feat: support azapi 2.0

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azapi = {
       source  = "azure/azapi"
-      version = "~> 1.9"
+      version = "~> 2.0"
     }
     modtm = {
       source  = "azure/modtm"
